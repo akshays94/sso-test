d8015d0b10abf88e75dca2bc056aa16f0f79288b
Akshay Suresh <akshays19294@gmail.com>
2020-03-05 17:32:13 +0530

    INTIAL COMMIT

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <button @click="navigate()">Login SSO</button>
+</div>
+</template>
+
+<script>
+export default {
+    created () {
+    },
+    methods: {
+        navigate () {
+            let url = 'https://rhombusdevteam-dev.onelogin.com/trust/saml2/http-post/sso/398e7049-05d0-4966-a3f9-23bbd542b665'
+            window.location.href = url
+        }
+    }
+}
+</script>
